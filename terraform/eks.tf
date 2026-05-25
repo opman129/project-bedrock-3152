f0df68fa47bddd6fcb91ac7a7e369d11264e6c1f
Opman129 <opemipojokotagba@gmail.com>
2026-05-25 09:18:05 +0100

    initiate eks.tf and kubernetes configs

---
+++ terraform/eks.tf
@@ -0,0 +1,39 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.33"
+
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    bedrock_nodes = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+
+      instance_types = ["t3.medium"]
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  cluster_enabled_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+
+  tags = {
+    Project = "karatu-2025-capstone"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "region" {
+  value = var.aws_region
+}
