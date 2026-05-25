1fedaec5fdf4f8cad6ef7bd51434f85851db7428
Opman129 <opemipojokotagba@gmail.com>
2026-05-25 15:28:24 +0100

    add redis.yaml

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -15,9 +15,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     bedrock_nodes = {
-      desired_size = 4
+      desired_size = 6
       min_size     = 2
-      max_size     = 4
+      max_size     = 8
 
       instance_types = ["t3.micro"]
 
